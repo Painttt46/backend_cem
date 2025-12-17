ecb466ab4e2583870fb63eb6696c04a38beee253
natsarin <natsarin@gent-s.com>
2025-12-17 16:24:31 +0700

    edit host

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost', 
   database: process.env.DB_NAME || 'SOCC',
-  password: process.env.DB_PASSWORD || '2546paint',
+  password: process.env.DB_PASSWORD || 'postgres',
   port: process.env.DB_PORT || 5432,
   
   // Pool configuration
