0eabc24d8074cf76c610dfb7f95ebf5d1042e5ea
Painttt46 <vpaint345@gmail.com>
2026-01-06 11:58:31 +0700

    FASF

--- routes/files.js
+++ routes/files.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 const upload = multer({ 
   storage: storage,
   limits: {
-    fileSize: 100 * 1024 * 1024 // 100MB limit
+    fileSize: 200 * 1024 * 1024 // 200MB limit
   }
 });
 
--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 
 // Static files - serve uploads folder
 app.use('/uploads', express.static('uploads'));
