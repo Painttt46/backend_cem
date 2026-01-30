1be2d12d0774f56fb07510dc08f21486724c05fe
Painttt46 <vpaint345@gmail.com>
2026-01-30 13:47:29 +0700

    V1

--- migrations/add_workflow_notification_flags.sql
+++
@@ -1,4 +0,0 @@
--- Add notification tracking columns to task_steps
-ALTER TABLE task_steps ADD COLUMN IF NOT EXISTS notified_start BOOLEAN DEFAULT FALSE;
-ALTER TABLE task_steps ADD COLUMN IF NOT EXISTS notified_due BOOLEAN DEFAULT FALSE;
-ALTER TABLE task_steps ADD COLUMN IF NOT EXISTS notified_overdue BOOLEAN DEFAULT FALSE;
